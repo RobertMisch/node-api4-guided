be15f40cd8d49340bc836fbe6c81a90412b18465
RobertMisch <robertmisch94@gmail.com>
2020-05-07 13:53:59 -0400

    deploying

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     const { authorization } = req.headers
-    const API_SECRET = process.env.API_SECRET //api_secret is a bannana word
+    const API_SECRET = process.env.API_SECRET || "banana" //api_secret is a banana word
 
     if (authorization === API_SECRET) {
         next()
