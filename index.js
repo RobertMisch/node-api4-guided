b05be8669db0278327649ff28526f68316bb8cce
RobertMisch <robertmisch94@gmail.com>
2020-05-07 12:41:09 -0400

    deploying

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+//heroku will add the PORT variable to the environment. then we have || 5000 for local usage
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
