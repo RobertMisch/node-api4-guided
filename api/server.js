5480284bf4416c5f52bd212040948dd0273fd20c
RobertMisch <robertmisch94@gmail.com>
2020-05-07 13:43:45 -0400

    added auth

--- api/server.js
+++ api/server.js
@@ -7,6 +7,11 @@ const server = express();
 
 server.use(helmet());
 
+//an example from lecture. should return as an empty object
+// server.get('/', function(req,res){
+//     res.status(200).json({environment: process.env.NODE_ENV})
+// })
+
 server.use('/api', apiRouter);
 
 module.exports = server;
