c5d7f30daaa87296962617af4b121ba47616d750
RobertMisch <robertmisch94@gmail.com>
2020-05-07 12:48:04 -0400

    checking if auto deploy works

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./api/server.js");
 
 //heroku will add the PORT variable to the environment. then we have || 5000 for local usage
+//adding this for a push to make sure heroku is doing automatic deploys
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
